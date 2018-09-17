e486e4f5ddad6f2d776c38850db762dc6a18d5ae
natemoo-re <ntmoore@knox.edu>
2018-09-16 22:43:27 -0500

    security audit

--- core/stencil.config.ts
+++ core/stencil.config.ts
@@ -1,7 +1,17 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'smore',
+  bundles: [
+    { components: ['smore-async-content'] },
+    { components: ['smore-counter'] },
+    { components: ['smore-typewriter'] },
+    { components: ['smore-window'] }
+  ],
+  plugins: [
+    sass()
+  ],
   outputTargets: [
     {
       type: 'dist'
